3b727a22a0f78782af898279bd8d1a3b3bca1e1a
Douglas C Machado <douglasclaudino10.dc@gmail.com>
2022-03-31 15:35:09 -0300

    v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,62 @@
-const userInput = document.querySelector('.user-input')
-let numberOne = Number(prompt("Digite o primeiro número:"))
-let numberTwo = Number(prompt("Digite o segundo número:"))
+function Calculator(){ //Constructor function
+  // catching display by class name inside of the function
+  this.display = document.querySelector('.display');
 
-function Calculator(numberOne, numberTwo){
-  this.plus = () =>{
-    return numberOne + numberTwo
+  this.start = () =>{
+    this.getUserClick()
+    this.pressEnter();
   }
 
-  this.minus = () =>{
-    return numberOne - numberTwo
+  //listening for keypress('Enter')
+  this.pressEnter = () =>{
+    document.addEventListener('keypress', e => {
+      if (e.keyCode === 13) this.calculate();
+    })
   }
 
-  this.times = () =>{
-    return numberOne * numberTwo
-  }
+  //action handler
+  this.getUserClick = () =>{
+    document.addEventListener('click', event => {
+      const el = event.target
+      if (el.classList.contains("btn-num")) this.addNumDisplay(el);
+      if (el.classList.contains('btn-clear')) this.clear();
+      if (el.classList.contains('btn-del')) this.del();
+      if (el.classList.contains('btn-eq')) this.calculate();
+    });
+  };
 
-  this.divided = () =>{
-    return numberOne / numberTwo
-  }
-};
+  //calculate function with a try, catch in case of error
+  this.calculate = () => {
+    try {
+      const conta = eval(this.display.value);
 
-document.addEventListener('click', function(e){
-  const el = e.target
-  if(el.classList.contains('plus')){
+      if(!conta) {
+        alert('Conta inválida');
+        return;
+      }
 
-    const result = new Calculator(numberOne, numberTwo)
-    document.querySelector(".result").innerHTML = result.plus()
-  }else if(el.classList.contains('minus')){
+      this.display.value = conta;
+      this.display.focus();
+    } catch(e) {
+      alert('Conta inválida');
+      return;
+    }
+  };
 
-    const result = new Calculator(numberOne, numberTwo)
-    document.querySelector(".result").innerHTML = result.minus()
-  }else if(el.classList.contains('times')){
+  //clearing the display value
+  this.clear = () => this.display.value = "";
 
-    const result = new Calculator(numberOne, numberTwo)
-    document.querySelector(".result").innerHTML = result.times()
-  }else if(el.classList.contains('divided')){
+  //this method erase the last digit
+  this.del = () => this.display.value = this.display.value.slice(0, -1);
 
-    const result = new Calculator(numberOne, numberTwo)
-    document.querySelector(".result").innerHTML = result.divided()
-  }
-})
+  //this method will show the key pressed in the display
+  this.addNumDisplay = (el) =>{
+    this.display.value += el.innerText;
+    this.display.focus();
+  };
+  
+}
+
+//initializing the function calling the start method
+let calculator = new Calculator()
+calculator.start()
